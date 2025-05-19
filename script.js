72f374767d65234d932997f054c4f05f17556969
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-05-19 17:18:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function mincost(arr){ 
 	arr.sort((a,b) => a-b);
 	let cost = 0;
-	let i = 0;
 	while(arr.length > 1){
-		let sum = arr[i] + arr[i+1];
+		let sum = arr[0] + arr[1];
 		cost += sum;
-		arr.shift();
-		arr.shift();
-		arr.unshift(sum);
-		arr.sort();
+		arr.splice(0,2);
+		arr.push(sum);
+		arr.sort((a,b) => a-b);
 	}
 	return cost;
   
